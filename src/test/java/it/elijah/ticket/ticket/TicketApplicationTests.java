f29d79c4064a4d116b9be7c44eff322b790f5aa7
Elijah <64620855+elijahit@users.noreply.github.com>
2024-07-24 15:03:38 +0200

    INIT: Inizializzato progetto maven per i ticket

---
+++ src/test/java/it/elijah/ticket/ticket/TicketApplicationTests.java
@@ -0,0 +1,13 @@
+package it.elijah.ticket.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
